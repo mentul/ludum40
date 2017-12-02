3c1cab49b78b5baee0329a381fd0a339b6d29b76
ZIEL0N <zielon94@gmail.com>
2017-12-02 20:30:23 +0100

    Mamut się rusza!

---
+++ Assets/Scripts/Mammoth_idle.cs
@@ -0,0 +1,46 @@
+﻿using StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Mammoth_idle : State
+    {
+        public float eatTime = 10f;
+        public float time;
+        Rigidbody2D rigidbody;
+
+        public override void Enter()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            print(gameObject.name + " entering idle");
+            time = eatTime;
+        }
+
+        public override void Execute()
+        {
+            if (time > 0) time -= Time.deltaTime;
+            else
+            {
+                stateMachine.ChangeState(GetStateOfType(typeof(Mammoth_wander)));
+            }
+            print(gameObject.name + " executing idle");
+        }
+
+        public override void Exit()
+        {
+            print(gameObject.name + " exiting idle");
+        }
+
+        public override bool OnMessage(GameObject gameObject, Message msg)
+        {
+            print(gameObject.name + " received " + msg.Subject);
+            return false;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Mammoth_wander.cs
@@ -0,0 +1,46 @@
+﻿using StateMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Mammoth_wander : State
+    {
+        public float wanderTime = 20f;
+        public float time;
+        Rigidbody2D rigidbody;
+
+        public override void Enter()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().velocity = Vector2.right;
+            print(gameObject.name + " entering wander");
+            time = wanderTime;
+        }
+
+        public override void Execute()
+        {
+            if (time > 0) time -= Time.deltaTime;
+            else
+            {
+                stateMachine.ChangeState(GetStateOfType(typeof(Mammoth_idle)));
+            }
+            print(gameObject.name + " executing wander");
+        }
+
+        public override void Exit()
+        {
+            print(gameObject.name + " exiting wander");
+        }
+
+        public override bool OnMessage(GameObject gameObject, Message msg)
+        {
+            print(gameObject.name + " received " + msg.Subject);
+            return false;
+        }
+        
+    }
+}
