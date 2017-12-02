936aeeeb30411ee37b80a52536ae2803fccdb214
ZIEL0N <zielon94@gmail.com>
2017-12-02 21:09:15 +0100

    SpriteOrdering

---
+++ Assets/Scripts/SpriteOrdering.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOrdering : MonoBehaviour {
+    SpriteRenderer renderer;
+    Collider collider;
+	void Start () {
+        collider = GetComponent<Collider>();
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sortingOrder = (int)((1f-Camera.main.WorldToViewportPoint(transform.position).y)*100f);
+	}
+	
+	void Update () {
+		if (transform.hasChanged)
+        {
+            renderer.sortingOrder = (int)((1f - Camera.main.WorldToViewportPoint(transform.position).y) * 100f);
+        }
+	}
+}
