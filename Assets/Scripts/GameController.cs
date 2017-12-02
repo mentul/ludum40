d8ec6359a0c3a9b447b6f914a00260d62e077e62
maRuda <aleksandra.lemiesz@gmail.com>
2017-12-02 16:13:42 +0100

    generowanie drzew

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public GameObject GeneratedMap;
+
 	// Use this for initialization
 	void Start () {
-		
+        GeneratedMap.GetComponent<GeneratedMap>().DoInit();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        GeneratedMap.GetComponent<GeneratedMap>().DoUpdate();
+    }
 }
