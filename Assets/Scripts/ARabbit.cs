f2b94ceaac9cf733d48b4d33f452fefddbe39dc1
Krzychu-Lab\Krzychu <dainarudd@gmail.com>
2017-12-02 19:27:27 +0100

    prefaby zwierząt i zmiany w prefabach krzaków

---
+++ Assets/Scripts/ARabbit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARabbit : Animal
+{
+
+	public override void DoInit ()
+	{
+		base.DoInit ();
+	}
+
+	public override void DoUpdate ()
+	{
+		base.DoUpdate ();
+	}
+}
---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+	public enum AnimalType { rabbit, elk, mammoth }
+	public enum AnimalState { hiding, sightseeing, fighting, vanishing }
+
+	public AnimalType animalType;
+	public AnimalState animalState;
+	public float minTimeToSpawn, maxTimeToSpawn, minTimeToVanish,maxTimeToVanish;
+	protected float timeToSpawn, timeToVanish;
+
+	public Collider2D walkCollider;
+	public Collider2D bodyTrigger;
+
+	public virtual void DoInit ()
+	{
+		timeToSpawn = Random.Range (minTimeToSpawn, maxTimeToSpawn);
+		timeToVanish = Random.Range (minTimeToVanish, maxTimeToVanish);
+	}
+
+	public virtual void DoUpdate ()
+	{
+		switch (animalState)
+		{
+			case AnimalState.hiding:
+				timeToSpawn -= Time.deltaTime;
+				if (timeToSpawn <= 0f)
+					animalState = AnimalState.sightseeing;
+				break;
+			case AnimalState.sightseeing:
+				timeToVanish -= Time.deltaTime;
+				if (timeToVanish <= 0f)
+					animalState = AnimalState.vanishing;
+				break;
+			case AnimalState.fighting:
+
+				break;
+			case AnimalState.vanishing:
+				Destroy (this.gameObject);
+				break;
+		}
+	}
+
+	public void OnDestroy ()
+	{
+
+	}
+
+}
