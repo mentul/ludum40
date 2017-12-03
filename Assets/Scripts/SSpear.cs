98445cf13d310b2c9390590c491c642c6cd5be45
ZIEL0N <zielon94@gmail.com>
2017-12-03 16:07:32 +0100

    Spear collision fix

--- Assets/Scripts/SSpear.cs
+++ Assets/Scripts/SSpear.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SSpear : MonoBehaviour {
+public class SSpear : MonoBehaviour
+{
 
     public Sprite secondSprite;
     public float timeToPickup = 1f;
     float time = 1f;
     public bool isActive = true;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         time = timeToPickup;
         isActive = true;
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("SpearPicking"), false);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if(GameController.isRunning)
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameController.isRunning)
         {
             GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity - GetComponent<Rigidbody2D>().velocity * Time.deltaTime * 0.5f;
-            if (GetComponent<Rigidbody2D>().velocity.magnitude < 1f && GetComponent<Rigidbody2D>().velocity.magnitude!=0f)
+            if (GetComponent<Rigidbody2D>().velocity.magnitude < 1f && GetComponent<Rigidbody2D>().velocity.magnitude != 0f)
             {
                 TurnOffTheSpear();
                 time = 0f;
@@ -32,10 +36,11 @@ public class SSpear : MonoBehaviour {
             }
             else time -= Time.deltaTime;
         }
-	}
+    }
 
     public void TurnOffTheSpear()
     {
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Default"), LayerMask.NameToLayer("SpearPicking"), true);
         isActive = false;
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         GetComponent<Rigidbody2D>().velocity = Vector2.zero;
@@ -45,7 +50,7 @@ public class SSpear : MonoBehaviour {
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.GetComponent<PlayerController>()!=null)
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
             collision.gameObject.GetComponent<PlayerController>().PickUpSpear();
             Destroy(gameObject);
